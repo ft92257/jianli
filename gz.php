50c8b06d69ca1adca9a43b5b8b14a561d36b0ff6
hds <hds_java@163.com>
2014-02-14 11:52:41 +0800

    hds

--- gz.php
+++ gz.php
@@ -13,81 +13,82 @@ echo yjl_html_gz_head($s, 'index');
 <div class="cen_div_cen">
  <div class="banner_img_div"><img src="images/banner_0_img0.png" width="980" height="300" /></div>
  <div class="of_ls_kj_alstyle">
-  <h1 class="titles_h titles_h_tubiao0">工装服务案例 连锁店</h1>
+  <h1 class="titles_h titles_h_tubiao0">工装服务案例 办公室</h1>
   <div class="anli_dl_list">
    <dl>
-    <dt><a href="photo-5811-190.html"><img src="images/anli_list_img0.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5794-195.html"><img src="images/办公室上投摩根.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="photo-5811-190.html">美吉生物</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5794-195.html">上投摩根 办公室</a>
+      <p></p>
     </dd>
    </dl>
    <dl>
-    <dt><a href="photo-5794-189.html"><img src="images/anli_list_img1.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5794-196.html"><img src="images/办公室huaaoxintuo.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="photo-5794-189.html">宝宜威机电</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5794-196.html">华澳国际 办公室</a>
+     <p></p>
     </dd>
    </dl>
    <dl>
-    <dt><a href="photo-5794-191.html"><img src="images/anli_list_img2.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5794-189.html"><img src="images/办公室baoyiwei.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="photo-5794-191.html">柯莱逊综合楼</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5794-189.html">宝宜威 办公楼 </a>
+     <p></p>
     </dd>
    </dl>
    <div class="clear"></div>
   </div>
  </div><!--/of_ls_kj_alstyle-->
  <div class="of_ls_kj_alstyle topmargin">
-  <h1 class="titles_h titles_h_tubiao1">工装服务案例 办工室</h1>
+  <h1 class="titles_h titles_h_tubiao1">工装服务案例 实验室</h1>
   <div class="anli_dl_list">
    <dl>
-    <dt><a href="photo-5811-190.html"><img src="images/anli_list_img3.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5811-190.html"><img src="images/实验室meiji.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="photo-5811-190.html">美吉生物</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5811-190.html">上海美吉生物 实验室  </a>
+     <p></p>
     </dd>
    </dl>
    <dl>
-    <dt><a href="#"><img src="images/anli_list_img4.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5811-197.html"><img src="images/实验室kelaixun.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="photo-5794-189.html">宝宜威机电</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5811-197.html">上海柯莱逊生物 实验室</a>
+     <p></p>
     </dd>
    </dl>
-   <dl>
+   <!-- <dl>
     <dt><a href="#"><img src="images/anli_list_img5.png" width="300" height="200" /></a></dt>
     <dd>
      <a href="photo-5794-191.html">柯莱逊综合楼</a>
      <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
     </dd>
    </dl>
+    -->
    <div class="clear"></div>
   </div>
  </div><!--/of_ls_kj_alstyle-->
  <div class="of_ls_kj_alstyle topmargin">
-  <h1 class="titles_h titles_h_tubiao2">工装服务案例 公共空间</h1>
+  <h1 class="titles_h titles_h_tubiao2">工装服务案例 连锁店</h1>
   <div class="anli_dl_list">
    <dl>
-    <dt><a href="#"><img src="images/anli_list_img6.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5803-199.html"><img src="images/连锁店zanshixiaoniao.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="#">公共空间</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5803-199.html">钻石小鸟 连锁品牌店</a>
+     <p></p>
     </dd>
    </dl>
    <dl>
-    <dt><a href="#"><img src="images/anli_list_img7.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5812-198.html"><img src="images/连锁店21shiji.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="#">公共空间</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5812-198.html">21世纪 连锁品牌店</a>
+     <p></p>
     </dd>
    </dl>
    <dl>
-    <dt><a href="#"><img src="images/anli_list_img8.png" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5803-200.html"><img src="images/连锁店disushangmao.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="#">公共空间</a>
-     <p>讲座介绍小空间设计案例。从楼阁到隐藏在小巷里的联排住宅，讲座介绍小空间设计案例。从楼阁到隐藏讲座介绍小空间设计案例。从楼阁到隐藏</p>
+     <a href="http://yijianli.com/photo-5803-200.html">某国际知名服装 连锁品牌店</a>
+     <p></p>
     </dd>
    </dl>
    <div class="clear"></div>
