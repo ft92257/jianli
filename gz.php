dc4717e2a0cc36f8832008b7fc217cba5f1d7597
hds <hds_java@163.com>
2014-02-14 14:11:16 +0800

    hds

--- gz.php
+++ gz.php
@@ -16,46 +16,56 @@ echo yjl_html_gz_head($s, 'index');
   <h1 class="titles_h titles_h_tubiao0">工装服务案例 办公室</h1>
   <div class="anli_dl_list">
    <dl>
-    <dt><a href="http://yijianli.com/photo-5794-195.html"><img src="images/bgsstmg.jpg" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5794-195.html"><img src="images/11212111.jpg" width="300" height="200" /></a></dt>
     <dd>
      <a href="http://yijianli.com/photo-5794-195.html">上投摩根 办公室</a>
-      <p></p>
+      <p>成立于2004年，位于浦东新区富城路99号震旦大厦19F，占地规模1400余平米，注册资本1.5亿人民币，致力于设立和管理证券投资基金，目前管理的资产规模已超过700亿元人民币。</p>
     </dd>
    </dl>
    <dl>
     <dt><a href="http://yijianli.com/photo-5794-196.html"><img src="images/huaaoxintuo.jpg" width="300" height="200" /></a></dt>
     <dd>
      <a href="http://yijianli.com/photo-5794-196.html">华澳国际 办公室</a>
-     <p></p>
+     <p>成立于1992年，位于浦东新区富城路99号震旦大厦9F，占地规模1400余平米，是由北京融达投资、北京三吉利能源和澳大利亚麦格理资本证券共同出资组建的中外合资非银行金融机构。</p>
     </dd>
    </dl>
    <dl>
     <dt><a href="http://yijianli.com/photo-5794-189.html"><img src="images/baoyiwei.jpg" width="300" height="200" /></a></dt>
     <dd>
      <a href="http://yijianli.com/photo-5794-189.html">宝宜威 办公楼 </a>
-     <p></p>
+     <p>成立于2001年，位于上海市新骏环路118号1号楼，占地规模1900余平米。宝宜威公司作为一家技术型公司，专注于力矩和装配技术。</p>
     </dd>
    </dl>
    <div class="clear"></div>
   </div>
  </div><!--/of_ls_kj_alstyle-->
  <div class="of_ls_kj_alstyle topmargin">
-  <h1 class="titles_h titles_h_tubiao1">工装服务案例 实验室</h1>
+  <h1 class="titles_h titles_h_tubiao1">工装服务案例 连锁店</h1>
   <div class="anli_dl_list">
-   <dl>
-    <dt><a href="http://yijianli.com/photo-5811-190.html"><img src="images/meiji.jpg" width="300" height="200" /></a></dt>
+  <dl>
+    <dt><a href="http://yijianli.com/photo-5803-199.html"><img src="images/zanshixiaoniao.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="http://yijianli.com/photo-5811-190.html">上海美吉生物 实验室  </a>
-     <p></p>
+     <a href="http://yijianli.com/photo-5803-199.html">钻石小鸟 连锁品牌店</a>
+     <p>成立于2002年，率先把“鼠标+水泥”的模式引入网络奢侈品销售，是国内最早的网络钻石品牌，倡导钻石文化的传播，坚持以安特卫普经典切割，为消费者带来购钻体验和DIY乐趣。</p>
     </dd>
    </dl>
    <dl>
-    <dt><a href="http://yijianli.com/photo-5811-197.html"><img src="images/kelaixun.jpg" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5812-198.html"><img src="images/21shiji.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="http://yijianli.com/photo-5811-197.html">上海柯莱逊生物 实验室</a>
-     <p></p>
+     <a href="http://yijianli.com/photo-5812-198.html">21世纪 连锁品牌店</a>
+     <p>成立于2006年，同时整体加盟21世纪不动产——上海区域分部，统一以“21世纪不动产——上海锐丰房地产”商号开展经营，目前已在上海市场拥有近170家门店。</p>
     </dd>
    </dl>
+   <dl>
+    <dt><a href="http://yijianli.com/photo-5803-200.html"><img src="images/disushangmao.jpg" width="300" height="200" /></a></dt>
+    <dd>
+     <a href="http://yijianli.com/photo-5803-200.html">地素时尚 连锁品牌店</a>
+     <p>成立于2002年，位于上海市丹巴路28弄—旭辉世纪广场8号楼，占地规模7500余平米，
+		公司定位清晰，现代化供应链、营销服务系统、智能网络管理模块等正全方位推动各品牌发展。
+     </p>
+    </dd>
+   </dl>
+   
    <!-- <dl>
     <dt><a href="#"><img src="images/anli_list_img5.png" width="300" height="200" /></a></dt>
     <dd>
@@ -68,27 +78,20 @@ echo yjl_html_gz_head($s, 'index');
   </div>
  </div><!--/of_ls_kj_alstyle-->
  <div class="of_ls_kj_alstyle topmargin">
-  <h1 class="titles_h titles_h_tubiao2">工装服务案例 连锁店</h1>
+  <h1 class="titles_h titles_h_tubiao2">工装服务案例 实验室</h1>
   <div class="anli_dl_list">
    <dl>
-    <dt><a href="http://yijianli.com/photo-5803-199.html"><img src="images/zanshixiaoniao.jpg" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5811-190.html"><img src="images/meiji.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="http://yijianli.com/photo-5803-199.html">钻石小鸟 连锁品牌店</a>
-     <p></p>
+     <a href="http://yijianli.com/photo-5811-190.html">上海美吉生物 实验室  </a>
+     <p>浦东办公环境最好的生物科技公司之一，位于上海张江高科瑞庆路，占地规模3861余平米，致力于致力于世界尖端生物技术开发与产业化领域，积极推动生物抗癌技术应用进步与发展。</p>
     </dd>
    </dl>
    <dl>
-    <dt><a href="http://yijianli.com/photo-5812-198.html"><img src="images/21shiji.jpg" width="300" height="200" /></a></dt>
+    <dt><a href="http://yijianli.com/photo-5811-197.html"><img src="images/kelaixun.jpg" width="300" height="200" /></a></dt>
     <dd>
-     <a href="http://yijianli.com/photo-5812-198.html">21世纪 连锁品牌店</a>
-     <p></p>
-    </dd>
-   </dl>
-   <dl>
-    <dt><a href="http://yijianli.com/photo-5803-200.html"><img src="images/disushangmao.jpg" width="300" height="200" /></a></dt>
-    <dd>
-     <a href="http://yijianli.com/photo-5803-200.html">某国际知名服装 连锁品牌店</a>
-     <p></p>
+     <a href="http://yijianli.com/photo-5811-197.html">上海柯莱逊生物 实验室</a>
+     <p>位于上海康新公路，占地规模3322余平米，公司拥有世界一流的测序平台，专注于新一代DNA高通量测序技术的应用和推广、生物信息分析、临床诊断试剂盒研发和转化医学研究。 </p>
     </dd>
    </dl>
    <div class="clear"></div>
