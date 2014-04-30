0a80d2c1488b7b7fec16e9d84c4771ddf16c05ab
ft92257 <419302316@qq.com>
2014-04-30 13:09:06 +0800

    t

--- gz.php
+++ gz.php
@@ -15,7 +15,7 @@ echo yjl_html_gz_head($s, 'index');
         	<ul class="big-imgbox" style="z-index:0;">
             	<li><a href="newnav.php?p=8"><img src="newnav/images/1ban.jpg" width="2560" height="300" /></a></li>
                 <li><a href="newnav.php?p=19"><img src="newnav/images/2ban.jpg" width="2560" height="300" /></a></li>
-                <li><a href="newnav.php?p=19"><img src="newnav/images/3ban.png" width="2560" height="300" /></a></li>
+                <li><a href="html.php?p=choose"><img src="newnav/images/3ban.png" width="2560" height="300" /></a></li>
                 <li><a href="newnav.php?p=9"><img src="newnav/images/4ban.jpg" width="2560" height="300" /></a></li>
             </ul>
             <div style="width:980px;margin-left:auto;margin-right:auto">
