a8b3b9ee9a182fc2fc4e0f23b36d88d4b1bb33e3
ft92257 <419302316@qq.com>
2014-05-09 14:47:55 +0800

    t

--- inspect/Home/Node/Lib/Action/BudgetAction.class.php
+++ inspect/Home/Node/Lib/Action/BudgetAction.class.php
@@ -52,6 +52,20 @@ class BudgetAction extends BaseAction {
 		PieImage::make($data);
 	}
 	
+	/*
+	 * 大饼图
+	*/
+	public function softpie() {
+		import('Public.Library.PieImage', './');
+	
+		$data = getRequest('data');
+		if (!empty($data)) {
+			$data = explode(",", $data);
+		}
+	
+		PieImage::soft($data);
+	}
+	
 	public function calculate() {
 		$acreage = (int) getRequest('acreage');
 		$budget = (int) getRequest('budget');
@@ -116,5 +130,116 @@ class BudgetAction extends BaseAction {
 		echo json_encode($arr);
 	}
 	
+	public function softCalculate() {
+		$acreage = (int) getRequest('acreage');
+		$budget = (int) getRequest('budget');
+		$grade = (int)getRequest('grade');
+	
+		if (!($acreage > 0)) {
+			die(json_encode(array('status' => 1001, 'msg' => '面积不能为空！')));
+		}
+	
+		if ($budget > 0) {
+			//已知预算和面积->各项档次
+			$total = $budget;
+			$price = $total / $acreage;
+			if ($price < 1200) {
+				$grade = 1;
+				$msg = '预算太低，可能不满足最低需求！';
+			} elseif ($price < 1600) {
+				$grade = 1;
+			} elseif ($price < 2500) {
+				$grade = 2;
+			} else {
+				$grade = 3;
+			}
+		} elseif ($grade > 0) {
+			//已知档次和面积->各项预算
+			$aPrice = array('1' => '1200', '2' => '1600', '3' => '2500');
+			$total = $aPrice[$grade] * $acreage;
+		} else {
+			die(json_encode(array('status' => 1002, 'msg' => '预算或档次至少填写一项！')));
+		}
+	
+		if ($grade == 1) {
+			$design_rate = 0.5 / 11;
+			$artificial_rate = 3 / 11;
+		} elseif($grade == 2) {
+			$design_rate = 1 / 14;
+			$artificial_rate = 3 / 14;
+		} elseif ($grade == 3) {
+			$design_rate = 1.5 / 25;
+			$artificial_rate = 3.5 / 25;
+		}
+		$design = round($total * $design_rate / 1000) * 1000;
+		$artificial = round($total * $artificial_rate / 1000) * 1000;
+		$material = $total - $design - $artificial;
+	
+		$arr = array(
+				'status' => 0,
+				'msg' => $msg ? $msg : '预估成功！',
+				'data' => array(
+						'design' => $design,
+						'artificial' => $artificial,
+						'material' => $material,
+						//'other' => 10000,
+						'grade' => array(
+								'design' => $grade,
+								'artificial' => $grade,
+								'material' => $grade,
+								//'other' => 2,
+						),
+				),
+		);
+		echo json_encode($arr);
+	}
+	
+	public function soft() {
+		$fields = array(
+				'electric' => array('家电', '#ff0000'),
+				'furniture' => array('家居', '#ffff00'),
+				'fabric' => array('布艺', '#0000ff'),
+				'green' => array('绿化', '#00ff00'),
+				'illumination' => array('照明', '#ff00ff'),
+				'furnishing' => array('陈设', '#00ffff'),
+			);
+		/*
+		if ($this->isPost()) {
+			$hard_budget = array(
+					'design' => (int) getRequest('design'),
+					'artificial' => (int) getRequest('artificial'),
+					'material' => (int) getRequest('material'),
+			);
+			$hard_budget['total'] = $hard_budget['design'] + $hard_budget['artificial'] + $hard_budget['material'];
+		} else {
+			//$hard_budget = $this->oUser->hard_budget;
+			//$hard_budget = json_decode($hard_budget, true);
+	
+			$hard_budget = array(
+					'design' => 11000,
+					'artificial' => 34000,
+					'material' => 115000,
+					'total' => 160000,
+			);
+		}
+		$acreage = 100;
+	
+		$this->assign('acreage', $acreage);
+		
+	*/
+		$hard_budget = array(
+				'electric' => '3000',
+				'furniture' => '2100',
+				'fabric' => '1400',
+				'green' => '700',
+				'illumination' => '1400',
+				'furnishing' => '1400',
+		);
+		
+		$this->assign('hard_budget', $hard_budget);
+		$this->assign('fields', $fields);
+		
+		$this->display();
+	}
 }
 ?>
---
+++ inspect/Home/Node/Runtime/Cache/4b716ac687b1835a880c4989d2c535ee.php
@@ -0,0 +1,166 @@
+<?php if (!defined('THINK_PATH')) exit();?>﻿<!doctype html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<script type="text/javascript" src="__STATICS__/js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="__STATICS__/js/common.js"></script>
+
+<title><?php echo ($Title); ?> | 易监理 - 装修行业中代表良心的力量，家装监理，连锁店装修监理，别墅装修监理，别墅监理，上海家装监理公司，家装监理公司，上海装修监理公司，家装监理，装潢监理，上海家装监理，上海装潢监理，上海装饰监理，上海装修监理，上海家庭装潢监理，装修监理，上海装修监理，验房，上海验房，家装监理师，装饰监理师，别墅监理，别墅装饰监理，家装工程监理，家庭装修监理，水电监理，家装监理费，家装施工监理，装修第三方监理</title>
+<meta name="keywords" content="易监理,上海家装监理公司,家装监理公司,上海装修监理公司,家装监理,装潢监理,上海家装监理,上海装潢监理,上海装饰监理,上海装修监理,上海家庭装潢监理,装修监理,上海装修监理,验房,上海验房,家装监理师,装饰监理师,别墅监理,别墅装饰监理,家装工程监理,家庭装修监理,水电监理,家装监理费,家装施工监理,装修第三方监理"/>
+<meta name="description" content="易监理，上海家装监理公司，家装监理公司，上海装修监理公司，家装监理，装潢监理，上海家装监理，上海装潢监理，上海装饰监理，上海装修监理，上海家庭装潢监理，装修监理，上海装修监理，验房，上海验房，家装监理师，装饰监理师，别墅监理，别墅装饰监理，家装工程监理，家庭装修监理，水电监理，家装监理费，家装施工监理，装修第三方监理"/>
+<meta charset="utf-8">
+<!–[if IE7 and IE10]>
+<style>
+ .topnav li{ float:left; cursor:pointer}
+ .topnav li a{ display:block; cursor:pointer}
+</style>
+<![endif]–>
+</head>
+<body>
+<iframe src="__HOST__/yjlnew.php" width="100%" height="135" frameborder=0 scrolling="no" target="_top" ></iframe>
+<script>
+	var URL = '__URL__';
+	var GROUP = '__GROUP__';
+</script>
+
+<div style="width:980px;margin:auto;">
+<h2>软装费用</h2>
+<style>
+	.pieTabs{
+		line-height:26px;
+	}
+	.pieTabs a {
+		color:#0000FF;
+	}
+</style>
+<div style="width:330px;float:left;">
+	<span class="pieTabs">
+		<a href="#" id="tab_forecast" style="color:red;">预计费用</a> | <a href="#" id="tab_my">我的预算</a> | <a href="#" id="tab_real">实际费用</a>
+	</span>
+	<img style="float:left" id="pie_image" src="<?php echo U('softpie');?>" />
+	<div style="float:left;margin-left:10px;line-height:28px;">
+	<?php if(is_array($fields)): foreach($fields as $field=>$vo): ?><span style="color:#ffffff;background:<?php echo ($vo[1]); ?>;"><?php echo ($vo[0]); ?></span> <span id="scale_<?php echo ($field); ?>"></span>% <br><?php endforeach; endif; ?>
+	</div>
+</div>
+<div style="650px;float:left;">
+建筑面积：<input type="text" id="acreage" name="acreage" value="<?php echo ($acreage); ?>" />㎡<br>
+设置预算：<input type="text" onblur="setBudget(this)" id="budget" name="budget" value="<?php echo ($hard_budget["total"]); ?>" />元<br>
+选择档次：<select name="grade" id="grade" onchange="selectGrade(this)"><option value="0">请选择档次</option><option value="3">高档</option><option value="2">中档</option><option value="1">低档</option></select><br>
+<button type="button" onclick="calculate()">预估费用</button><br><br>
+</div>
+
+<div style="float:left;width:980px;border-top:1px dashed #999;padding-top:10px;margin-top:5px;">
+<form method="post" action="">
+<table>
+	<tr><td width="100">名称</td><td width="100">预计费用</td><td width="200">我的预算</td><td width="100">档次</td><td width="100">实际已花费</td></tr>
+	<?php if(is_array($fields)): foreach($fields as $field=>$vo): ?><tr><td><?php echo ($vo[0]); ?></td><td><span id="fee_<?php echo ($field); ?>">0.00</span>元</td><td><input type="text" id="<?php echo ($field); ?>" name="<?php echo ($field); ?>" value="<?php echo ($hard_budget[$field]); ?>" />元</td><td id="grade_<?php echo ($field); ?>"><span id="grade_<?php echo ($field); ?>_3">高</span> <span id="grade_<?php echo ($field); ?>_2">中</span> <span id="grade_<?php echo ($field); ?>_1">低</span></td><td><span id="real_<?php echo ($field); ?>">0.00</span>元</td></tr><?php endforeach; endif; ?>
+	<tr><td colspan="5" style="border-top:1px solid #ccc;"></td></tr>
+	<tr><td>总费用</td><td><span id="fee_total">0.00</span>元</td><td><input type="checkbox" id="synBudget" onclick="synCheck(this)" />同步我的预算</td><td></td><td>0.00元</td></tr>
+</table>
+<br>
+<input type="submit" value="保 存" />
+</form>
+</div>
+
+</div>
+<script>
+
+function calculate(){
+	$.post(URL+'/calculate', {acreage:$('#acreage').val(), budget:$('#budget').val(), grade:$('#grade').val()}, function(json){
+		if (json.status == 0) {
+			var data = json.data;
+			var oGrade = {"1":"低", "2":"中", "3":"高"};
+			
+			var total = 0;
+			var strData = '';
+		<?php if(is_array($fields)): foreach($fields as $field=>$vo): ?>$("#fee_<?php echo ($field); ?>").html(data.<?php echo ($field); ?>);
+			if ($("#synBudget")[0].checked) {
+				$("#<?php echo ($field); ?>").val(data.<?php echo ($field); ?>);
+			}
+			$("#grade_<?php echo ($field); ?>").html(oGrade[data.grade.<?php echo ($field); ?>]);
+
+			strData += data.<?php echo ($field); ?> + ",";
+			total += parseInt(data.<?php echo ($field); ?>);<?php endforeach; endif; ?> 
+		
+			total = total / 100;
+			
+			var scale_artificial = Math.round(data.artificial/total);
+			//var scale_material = Math.round(data.material/total);
+			var scale_material =  100 - scale_design - scale_artificial;
+		
+		<?php if(is_array($fields)): foreach($fields as $field=>$vo): ?>var scale_<?php echo ($field); ?> = Math.round(data.design/total);
+			$("#scale_<?php echo ($field); ?>").html(scale_<?php echo ($field); ?>);<?php endforeach; endif; ?> 
+		
+			strData = strData.substr(0, strData.length - 1);
+
+			$("#fee_total").html(total * 100);
+			
+			$("#pie_image").attr("src", "<?php echo U('pie');?>/data/" + strData);
+		
+			$(".pieTabs a").css("color", 'blue');
+			$("#tab_forecast").css("color", 'red');
+		} else {
+			alert(json.msg);
+		}
+	}, 'json');
+}
+
+function selectGrade(obj) {
+	if (obj.value) {
+		$("#budget").val('');
+		calculate();
+	}
+}
+
+function setBudget(obj) {
+	if (obj.value) {
+		$("#grade").val(0);
+	}
+}
+
+function synCheck(obj) {
+	if (obj.checked) {
+		$("#design").val($("#fee_design").html());
+		$("#artificial").val($("#fee_artificial").html());
+		$("#material").val($("#fee_material").html());
+	}
+}
+
+$(function(){
+	calculate();
+	$(".pieTabs a").click(function(){
+		$(".pieTabs a").css("color", 'blue');
+		$(this).css("color", 'red');
+		
+	});
+	$("#tab_forecast").click(function(){
+		_updatePie({"design":$("#fee_design").html(), "artificial":$("#fee_artificial").html(), "material":$("#fee_material").html()});
+	});
+	$("#tab_my").click(function(){
+		_updatePie({"design":$("#design").val(), "artificial":$("#artificial").val(), "material":$("#material").val()});
+	});
+	$("#tab_real").click(function(){
+		_updatePie({"design":$("#real_design").html(), "artificial":$("#real_artificial").html(), "material":$("#real_material").html()});
+	});
+});
+
+function _updatePie(data) {
+	data.design = parseInt(data.design);
+	data.artificial = parseInt(data.artificial);
+	data.material = parseInt(data.material);
+	
+	$("#pie_image").attr("src", "<?php echo U('pie');?>/data/" + data.design + "," + data.artificial + "," + data.material);
+	var total = (data.design + data.artificial + data.material) / 100;
+	var scale_design = Math.round(data.design/total);
+	var scale_artificial = Math.round(data.artificial/total);
+	var scale_material =  100 - scale_design - scale_artificial;
+	
+	$("#scale_design").html(scale_design);
+	$("#scale_artificial").html(scale_artificial);
+	$("#scale_material").html(scale_material);
+}
+</script>
+
+
+</body>
+</html>
